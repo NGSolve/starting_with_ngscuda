e442b90ce76ec839429b889c35e0525d18d4db1c
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2025-11-30 22:53:34 +0100

    simplify demo_vecs

--- src/demo_vecs.cpp
+++ src/demo_vecs.cpp
@@ -1,32 +1,24 @@
 // ngs_nvcc -x cu -c  demo_vecs.cpp ; ngs_nvcc -dlink demo_vecs.o -o demo_vecs_device.o -L/opt/cuda/lib64 -L/home/jschoeberl/local_install/lib -lngcore ;  ngsld demo_vecs.o demo_vecs_device.o  -L/opt/cuda/lib64 -lcudart -lcudadevrt -lngcore
 
 
-#include "cuda_linalg.hpp"
+#include <cuda_ngbla.hpp>
 
-
-namespace ngla
-{
-
-  void Func()
-  {
-    Vector<double> x(10), y(10);
-    for (int i = 0; i < x.Size(); i++) x(i) = 10+i;
-    
-    Vector<Dev<double>> vx(x), vy(10);
-    
-    cout << "x = " << x << endl;
-    
-    vy.Range(0,10) = 4.*vx - vx;
-    vy.D2H (y);
-    
-    cout << "y = " << y << endl;
-  }
-
-  
-}
+using namespace ngs_cuda;
+using namespace ngbla;
 
 
 int main()
 {
-  ngla::Func();
+  Vector<double> x(10), y(10);
+  for (int i = 0; i < x.Size(); i++) x(i) = 10+i;
+  
+  ngbla::Vector<Dev<double>> vx(x), vy(10);
+    
+  cout << "x = " << x << endl;
+  
+  vy.Range(0,10) = 4.*vx - vx;
+  vy.D2H (y);
+  
+  cout << "y = " << y << endl;
 }
+
